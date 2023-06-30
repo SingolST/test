e24cfa76dd4a1d947f7d71e75ab94c5ae0192486
Stas Tsoy <singol.draft@gmail.com>
2023-06-30 11:03:56 +0500

    with desktop version

--- index.js
+++ index.js
@@ -11,14 +11,19 @@
       rot = document.getElementById('rotate');
   
     init = function() {
-      rot.addEventListener("touchstart", start, false);
-      $(document).bind('touchmove', function(event) {
+      var isMobile = /iPhone|iPad|iPod|Android|Windows Phone/i.test(navigator.userAgent);
+      var startEvent = isMobile ? 'touchstart' : 'mousedown';
+      var moveEvent = isMobile ? 'touchmove' : 'mousemove';
+      var endEvent = isMobile ? 'touchend' : 'mouseup';
+  
+      rot.addEventListener(startEvent, start, false);
+      document.addEventListener(moveEvent, function(event) {
         if (active === true) {
           event.preventDefault();
           rotate(event);
         }
       });
-      $(document).bind('touchend', function(event) {
+      document.addEventListener(endEvent, function(event) {
         event.preventDefault();
         stop(event);
       });
@@ -36,17 +41,32 @@
         x: l + (w / 2),
         y: t + (h / 2)
       };
-      x = e.touches[0].clientX - center.x;
-      y = e.touches[0].clientY - center.y;
+  
+      if (e.type === 'touchstart') {
+        x = e.touches[0].clientX - center.x;
+        y = e.touches[0].clientY - center.y;
+      } else {
+        x = e.clientX - center.x;
+        y = e.clientY - center.y;
+      }
+  
       startAngle = R2D * Math.atan2(y, x);
       return active = true;
     };
   
     rotate = function(e) {
       e.preventDefault();
-      var x = e.touches[0].clientX - center.x,
-        y = e.touches[0].clientY - center.y,
-        d = R2D * Math.atan2(y, x);
+      var x, y;
+  
+      if (e.type === 'touchmove') {
+        x = e.touches[0].clientX - center.x;
+        y = e.touches[0].clientY - center.y;
+      } else {
+        x = e.clientX - center.x;
+        y = e.clientY - center.y;
+      }
+  
+      var d = R2D * Math.atan2(y, x);
       rotation = d - startAngle;
   
       let val = angle + rotation % 360;
